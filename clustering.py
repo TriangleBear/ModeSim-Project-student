45e7f4685c5d56176b45051e181bba0b141be699
TriangleBear <kakanini31@gmail.com>
2024-04-29 12:26:16 +0800

    Separate Scatter plots side by side

--- clustering.py
+++ clustering.py
@@ -1,5 +1,10 @@
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
+from matplotlib.pyplot import subplots_adjust
+from matplotlib.cm import ScalarMappable
+import numpy as np
+from matplotlib.cm import ScalarMappable
 
 def cluster_students(X):
     """
@@ -18,6 +23,25 @@ def cluster_students(X):
     return labels
 
 def display_cluster_graph(X, clusters):
-    plt.scatter(X['Intell Ag Attention Span'], X['Chem Attention Span'], c=clusters)
-    plt.title('Cluster Graph')
+    plt.figure()
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Combine all attention span data into a single list
+    all_attention_span = X[['Intell Ag Attention Span', 'Chem Attention Span', 'Ethics Attention Span', 'Info Assurance Attention Span']].values.flatten()
+    plt.subplots_adjust(right=0.8)  
+
+    sm = ScalarMappable(norm=plt.Normalize(min(all_attention_span), max(all_attention_span)))
+    sm.set_array([])
+
+    # Plot Chemistry and Ethics as minor subjects
+    ax1.scatter(X['Chem Attention Span'], X['Ethics Attention Span'], c=clusters)
+
+    # Plot Intelligence Agency and Information Assurance as major subjects
+    ax2.scatter(X['Intell Ag Attention Span'], X['Info Assurance Attention Span'], c=clusters)
+
+    plt.colorbar(sm, label='Attention Span (Combined)', ax=[ax1, ax2])
+
+    ax1.set_title('Cluster Graph (Minor Subjects)')
+    ax2.set_title('Cluster Graph (Major Subjects)')
+
     plt.show()
